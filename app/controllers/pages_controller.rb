7d6d0b722e1ef28a1c63ae913d327c1f3f7cb2a0
Ethan Suttner <ethan.suttner@gmail.com>
2016-04-20 20:30:33 -0400

    initializing bootstrap

---
+++ app/controllers/pages_controller.rb
@@ -0,0 +1,8 @@
+class PagesController < ApplicationController
+  def home
+  end
+  def Engineer
+  end
+  def Resume
+  end
+end
--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,6 @@
 Rails.application.routes.draw do
+  root "pages#home"
+  get "/Engineer" => "pages#Engineer"
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
